fc0ede3b8bdf195a68a922cb7f443943c423f9a7
Sea-Deep <seadeepie@gmail.com>
2024-01-26 17:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,34 +25,30 @@ client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   if (message.content.includes("t")) i++;
 
-  const prefixRegex = /^(vito|pajeeto)\s+/i;
+  if (
+    message.content.toLowerCase().startsWith(`vito talk`) ||
+    message.mentions.users.first()?.id == client.user.id ||
+    i === 10
+  ) {
+    i = i === 10 ? 0 : i;
+    const msg = messages[Math.floor(Math.random() * messages.length)];
+    return message.channel.send(msg);
+  }
 
-  if (prefixRegex.test(message.content)) {
-    const command = message.content.replace(prefixRegex, "").toLowerCase();
-
-    if (command.startsWith("talk") || (command.startsWith("say") && message.content.includes("say")) || i === 10) {
-      i = i === 10 ? 0 : i;
-
-      if (command.startsWith("talk")) {
-        const msg = messages[Math.floor(Math.random() * messages.length)];
-        return message.channel.send(`${command} ${msg}`);
-      } else if (command.startsWith("say")) {
-        let msg = message.content.split(" ").splice(2).join(" ");
-        if (msg == "") {
-          return message.reply("```Cannot send message because the message provided is undefined.```");
-        }
-
-        try {
-          await message.delete();
-        } catch (e) {
-          console.log(e.message);
-        }
-
-        return message.channel.send(msg);
-      }
+  if (message.content.toLowerCase().startsWith(`vito say`)) {
+    let msg = message.content.split(" ").splice(2).join(" ");
+    if (msg == "") {
+      return message.reply(
+        "```Cannot send message because the message provided is undefined.```",
+      );
     }
+    try {
+      await message.delete();
+    } catch (e) {
+      console.log(e.message);
+    }
+    return message.channel.send(msg);
   }
 });
-
 
 client.login(process.env.TOKEN);
