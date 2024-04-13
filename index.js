f3cc4cdfcafb9256af070b22e1832228767ea7be
Sea-Deep <seadeepie@gmail.com>
2024-04-13 18:14:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const client = new Client({
   partials: [Partials.Channel],
 });
 
-
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity({
@@ -19,46 +18,38 @@ client.on("ready", async () => {
   });
   import("./form.js");
 
-let i;
-client.on('messageCreate', async (message) => {
+  let messageCounter = 0;
+
+  client.on('messageCreate', async (message) => {
     if (message.author.bot || message.content.trim() === '') return;
 
-   if (message.content.toLowerCase().includes("t")) i++;
+    if (message.content.toLowerCase().includes("t")) messageCounter++;
 
     if (
       message.content.toLowerCase().startsWith(`vito talk`) ||
-      i === 20
+      messageCounter === 20
     ) {
-      i = i === 20 ? 0 : i;
-      const msg = messages[Math.floor(Math.random() * messages.length)];
-      return message.channel.send(msg);
+      messageCounter = messageCounter === 20 ? 0 : messageCounter;
+      const randomMsg = getRandomMessage(messages);
+      return message.channel.send(randomMsg);
     }
 
     if (message.content.toLowerCase().startsWith(`vito say`)) {
-      let msg = message.content.split(" ").splice(2).join(" ");
-      if (msg == "") {
-        return message.reply(
-          "```Cannot send message because the message provided is undefined.```",
-        );
+      const msgContent = message.content.split(" ").splice(2).join(" ");
+      if (!msgContent) {
+        return message.reply("```Cannot send message because the message provided is undefined.```");
       }
       try {
         await message.delete();
-      } catch (e) {
-        console.log(e.message);
+      } catch (error) {
+        console.log(error.message);
       }
-      return message.channel.send(msg);
+      return message.channel.send(msgContent);
     }
   });
 });
 
-function getRandomPhrase(sentence) {
-  let words = sentence.split(" ");
-  let startingWordPos = rnum(words.length);
-  let phraseLength = rnum(4)+1;
-  let phrase = [];
-  for (let i = 0; i < phraseLength; i++) {
-    if (startingWordPos == words.length) break;
-     phrase.push(words[startingWordPos + i]);
-  }
-  return phrase.join(" ").trim();
+function getRandomMessage(messageArray) {
+  const randomIndex = Math.floor(Math.random() * messageArray.length);
+  return messageArray[randomIndex];
 }
